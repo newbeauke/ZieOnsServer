8cdb37b8ad5dc8f853b3167bfe5cbaaa5c8e275a
LucRademakers <112711738+LucRademakers@users.noreply.github.com>
2024-01-26 15:09:40 +0100

    Add files via upload
    Code voor de ESP32, hierbij kan de afbeelding worden verstuurd naar de MongoDB. 
    De base64-string komen zonder plustekens aan in de MongoDB!

---
+++ ESP32/ESP32_CAM(Encoded).ino
@@ -0,0 +1,178 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "esp_camera.h"
+#include "img_converters.h"
+#include <base64.h>
+
+const char* ssid = "iPhone van Luc";
+const char* password =  "R@demakers1";
+
+const char* serverName = "http://172.20.10.9:5000/api/Posters/Create";
+
+String baseName = "Image"; //--> NIET WEGDOEN!
+int imageNumber = 1;
+
+// Pin definition for CAMERA_MODEL_AI_THINKER
+// Change pin definition if you're using another ESP32 with camera module
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+//#define LED_BUILTIN        4 //--> ALLEEN NODIG VOOR LED!
+
+//Stores the camera configuration parameters
+camera_config_t config;
+
+void setup() {
+  Serial.begin(115200);
+  delay(4000);
+
+  //Initialize the camera  
+  Serial.print("Initializing the camera module...");
+  configInitCamera();
+  Serial.println("Ok!");
+
+  // Initialize the LED pin as an output
+  //pinMode(LED_BUILTIN, OUTPUT); //--> ALLEEN NODIG VOOR LED!
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void configInitCamera(){
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; //YUV422,GRAYSCALE,RGB565,JPEG
+
+  // Select lower framesize if the camera doesn't support PSRAM
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10; //10-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize the Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+   sensor_t * s = esp_camera_sensor_get();
+   s->set_brightness(s, 2);     // -2 to 2
+   s->set_contrast(s, 0);       // -2 to 2
+   s->set_saturation(s, 0);     // -2 to 2
+   s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+   s->set_whitebal(s, 0);       // 0 = disable , 1 = enable
+   s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+   s->set_wb_mode(s, 4);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+   s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+   s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+   s->set_ae_level(s, 0);       // -2 to 2
+   s->set_aec_value(s, 300);    // 0 to 1200
+   s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+   s->set_agc_gain(s, 0);       // 0 to 30
+   s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+   s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+   s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+   s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+   s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+   s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+   s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+   s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+   s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
+}
+
+void loop() {
+  if ((WiFi.status() == WL_CONNECTED)) { 
+    
+    //digitalWrite(LED_BUILTIN, HIGH); // LED AANZETTEN
+    //delay(3000); 
+    //digitalWrite(LED_BUILTIN, LOW);  // LED UITZETTEN
+    //delay(3000);
+    
+    camera_fb_t * fb = NULL;
+
+    // Take Picture with Camera
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+
+    if(fb->len > 0) {
+      Serial.println("Photo captured successfully");
+    } else {
+      Serial.println("Photo capture failed");
+    }   
+  	
+    String base64Encoded = base64::encode(fb->buf, fb->len);
+    Serial.println(base64Encoded);
+
+    WiFiClient client; 
+    HTTPClient http; 
+    http.begin(client, serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    String imageName = baseName + String(imageNumber++);
+    String httpRequestData = "&Name=" + imageName + "&Image=" + String(base64Encoded);
+
+    int httpResponseCode = http.POST(httpRequestData);
+
+    //return the frame buffer back to the driver for reuse
+    esp_camera_fb_return(fb); 
+
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(10000);  
+}
